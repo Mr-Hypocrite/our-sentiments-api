84c8916ab27714aafe3897345d4a35077de579bb
Mr-Hypocrite <ym.jatin@gmail.com>
2023-01-19 19:31:00 +0530

    feat: product controllers

---
+++ controllers/productControllers.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { loadPage, scrapeProductData, scrapeProducts } from "../helpers";
+
+export const getProducts = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const { productName } = req.params;
+    const products = await scrapeProducts( productName );
+    res.send( products );
+};
+
+export const getProduct = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const { productId } = req.params;
+    const productData = await scrapeProductData( productId );
+    res.send( productData );
+};
+
+export const getProductReviews = async (
+    req?: Request,
+    res?: Response,
+    next?: NextFunction
+) => {};
